35d214de44d73375d695f7b6b9a3a80ea69322e6
vedantmahawar <vedantmahawar@outlook.com>
2025-05-08 12:27:53 -0700

    Got basic collision working

--- main/level_0.gd
+++ main/level_0.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-var start_x = 200
-var start_y = 200
+var start_x = 40
+var start_y = 27
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
