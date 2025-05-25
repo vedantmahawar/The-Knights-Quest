f5c918f9360b5c050b13aa8604955f5f3146d809
vedantmahawar <vedantmahawar@outlook.com>
2025-05-25 10:39:39 -0700

    Add some more stuff

--- Better/the-knight's-quest/scripts/killzone.gd
+++ Better/the-knight's-quest/scripts/killzone.gd
@@ -2,10 +2,18 @@ extends Area2D
 
 @onready var timer: Timer = $Timer
 
-# Function to start timer
+# Function on death
 func _on_body_entered(body: Node2D) -> void:
+	# Start timer to reset game
 	timer.start()
+	# Slow everything down
+	Engine.time_scale = 0.5
+	# Make player fall through world
+	body.get_node("CollisionShape2D").queue_free()
 
 # Function to restart game
 func _on_timer_timeout() -> void:
+	# Restart Game
 	get_tree().reload_current_scene()
+	# Speed everything back to the proper speed 
+	Engine.time_scale = 1
--- Better/the-knight's-quest/scripts/player.gd
+++ Better/the-knight's-quest/scripts/player.gd
@@ -1,22 +1,40 @@
 extends CharacterBody2D
 
-
+# Definse constats
 const SPEED = 100
 const JUMP_VELOCITY = -300
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
+# Do the physics
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Add the gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# Handle jump
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# Get the input direction 
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	# Flips sprite
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
+	
+	# Play animation
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite_2d.play("idle")
+		else:
+			animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("jump")
+	
+	# Applies the movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
